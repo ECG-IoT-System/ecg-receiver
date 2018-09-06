af91a3142433aa2866896115d9e19612b22d92b5
Akiicat <aaaa1379@gmail.com>
2018-09-06 15:37:57 +0800

    set all time packet to 0 #1

--- models/timer.js
+++ models/timer.js
@@ -1,4 +1,5 @@
 const now = require('nano-time');
+const BigNumber = require('big-number');
 
 module.exports = class Timer {
   constructor() {}
@@ -12,16 +13,43 @@ module.exports = class Timer {
     this.diff = Math.round(this.hrend[0] + this.hrend[1] / 1e4);
   }
 
-  toJson() {
-    return {
-      time: new Date().toISOString(),
+  time() {
+    return BigNumber('1514862245006007008');
+  }
+
+  snap() {
+    // var time = BigNumber(now().toString());
+    var date = new Date(
+      parseInt(
+        this.time()
+          .div(1e6)
+          .toString(),
+      ),
+    );
+    this.current = {
+      time: this.time().toString(),
+      format: [this.time().div(1e9), this.time().mod(1e9)],
+      string: {
+        iso: date.toISOString(),
+        utc: date.toUTCString(),
+        gmt: date.toGMTString(),
+      },
+      resolution: {
+        zone: date.getTimezoneOffset(),
+        year: date.getUTCFullYear(),
+        month: date.getUTCMonth(),
+        date: date.getUTCDate(),
+        day: date.getUTCDay(),
+        hour: date.getUTCHours(),
+        min: date.getUTCMinutes(),
+        sec: date.getUTCSeconds(),
+        ms: date.getUTCMilliseconds(),
+        us: this.time()
+          .mod(1e6)
+          .div(1e3),
+        ns: this.time().mod(1e3),
+      },
       diff: this.diff,
-      hour: this.hour,
-      tick: this.tick,
-      min: Math.floor(this.tick / 100 / 1000 / 60),
-      sec: Math.floor((this.tick / 100 / 1000) % 60),
-      ms: Math.floor((this.tick / 100) % 1000),
-      us: Math.floor((this.tick * 10) % 1000),
     };
   }
 
@@ -29,21 +57,33 @@ module.exports = class Timer {
   // 4 bytes unsigned integer: hour
   // 4 bytes unsigned integer: tick (10us)
   toBuffer() {
-    this.current = now();
-    this.hour = new Date(this.current / 1e6).getUTCHours();
-    this.tick = Math.round((this.current / 1e4) % 3.6e8);
+    // can not fix issue #1 on receiver side, so don't waste time on the function before the bug is fixed.
+    return new Buffer(12);
 
-    var timeBuf = [this.diff, this.hour, this.tick];
+    this.snap();
+
+    var tick = BigNumber(this.current.resolution.minute)
+      .mult(60)
+      .add(this.current.resolution.sec)
+      .mult(1000)
+      .add(this.current.resolution.ms)
+      .mult(1000)
+      .add(this.current.resolution.us)
+      .div(10)
+      .toString();
+
+    var hour = this.current.resolution.hour;
+
+    var timeBuf = [this.diff, hour, tick];
 
     timeBuf = timeBuf.map(time => {
       let result = new Buffer(4);
-      result.writeUInt32LE(time);
+      result.writeUInt32LE(parseInt(time));
       return result;
     });
 
-    // debug
-    console.log(this.toJson());
+    // console.log(this.current);
 
-    return new Buffer.concat(timeBuf);
+    // return new Buffer.concat(timeBuf);
   }
 };
