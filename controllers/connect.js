e9df79c8f68255796015c90a5de4d2f7cb8605ca
Akiicat <aaaa1379@gmail.com>
2018-12-17 17:15:38 +0800

    add nest server

--- controllers/connect.js
+++ controllers/connect.js
@@ -130,10 +130,10 @@ module.exports = async function(peripheral) {
 
         if (_signals.length !== 256) return console.log('data.length is', _signals.length, '. not equal 256');
 
-        phpserver.send(_timediff, _signals, _gsensor, mac, rssi);
+        // phpserver.send(_timediff, _signals, _gsensor, mac, rssi);
         // phpRssiserver.sendRssi(_timediff, _signals, mac, rssi);
-        nodeserver.send(_timediff, _signals, _gsensor, mac, rssi);
-        // nestserver.send(_timediff, _signals, _gsensor, mac, rssi);
+        // nodeserver.send(_timediff, _signals, _gsensor, mac, rssi);
+        nestserver.send(_timediff, _signals, _gsensor, mac, rssi);
       });
 
       signals = [];
